179157baf1ec97764356dde28594192b005d68e5
thiswind <23764+thiswind@users.noreply.github.com>
2025-12-02 07:34:11 +0000

    增加了 /about

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello, World!"
 
+@app.route('/about')
+def about():
+    return 'This is the About Page.'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=55000)
